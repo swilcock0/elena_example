2d9f6ce167bb187ad8bdcbcaf1cee8c4893edf34
Sam <sam.wilcock0@gmail.com>
2025-04-17 18:27:51 +0200

    Edit to add shared memory gui version

--- load_robot_pb.py
+++ load_robot_pb.py
@@ -1,13 +1,83 @@
 import pybullet as p
 import pybullet_data
 import os
+import sys
+import time
+
 
 # Connect to the physics server using SharedMemory GUI
-physicsClient = p.connect(p.GUI_SERVER)
-if physicsClient < 0:
-    physicsClient = p.connect(p.GUI) # Fall back to regular GUI if SharedMemory fails
-    print("Falling back to regular GUI mode")
+# physicsClient = p.connect(p.GUI_SERVER)
+# if physicsClient < 0:
+#     physicsClient = p.connect(p.GUI) # Fall back to regular GUI if SharedMemory fails
+#     print("Falling back to regular GUI mode")
 
+class HideOutput(object):
+    '''
+    A context manager that block stdout for its scope, usage:
+
+    with HideOutput():
+        os.system('ls -l')
+    '''
+    DEFAULT_ENABLE = True
+    def __init__(self, enable=None):
+        if enable is None:
+            enable = self.DEFAULT_ENABLE
+        self.enable = enable
+        if not self.enable:
+            return
+        sys.stdout.flush()
+        self._origstdout = sys.stdout
+        self._oldstdout_fno = os.dup(sys.stdout.fileno())
+        self._devnull = os.open(os.devnull, os.O_WRONLY)
+
+    def __enter__(self):
+        if not self.enable:
+            return
+        self._newstdout = os.dup(1)
+        os.dup2(self._devnull, 1)
+        os.close(self._devnull)
+        sys.stdout = os.fdopen(self._newstdout, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not self.enable:
+            return
+        sys.stdout.close()
+        sys.stdout = self._origstdout
+        sys.stdout.flush()
+        os.dup2(self._oldstdout_fno, 1)
+        os.close(self._oldstdout_fno) # Added
+
+def run_shared_physics():
+    import subprocess
+    DETACHED_PROCESS = 8
+    executable = "Bullet_shared_GUI.exe"
+    subprocess.Popen(executable, creationflags=DETACHED_PROCESS)
+    
+def Disconnect():
+    ''' Disconnect any and all instances of bullet '''
+    with HideOutput():
+        for i in range(100):
+            try:
+                p.disconnect(i)
+            except:
+                break # Don't whinge about non-existent servers   
+    
+    
+
+    
+    
+    
+# Disconnect any existing connections to Bullet
+Disconnect()
+# Run the shared physics server in a detached process
+run_shared_physics()
+time.sleep(1)
+# Connect to the physics server using SharedMemory GUI
+physicsClient = p.connect(p.SHARED_MEMORY)
+ 
+    
+    
+    
 # Get the current script directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -26,6 +96,7 @@ planeId = p.loadURDF("plane.urdf")
 
 # Load the robot
 robot_urdf = os.path.join(urdf_path, "crx10ia_l.urdf")
+print(robot_urdf)
 robotBasePosition = [0, 0, 1]
 robotBaseOrientation = p.getQuaternionFromEuler([0, 0, 0])
 robotId = p.loadURDF(robot_urdf, robotBasePosition, robotBaseOrientation)
@@ -54,13 +125,13 @@ constraintId = p.createConstraint(
 target_positions = [0 for i in range(num_joints)]  # Example positions
 
 # Control parameters
-kp = 0.05  # Proportional gain
+kp = 0.7  # Proportional gain
 kv = 0.5  # Velocity gain
 max_force = 10000  # Maximum force to apply
 # Add joint damping to prevent oscillations
-# damping = 1
-# for i in range(num_joints):
-#     p.changeDynamics(robotId, i, linearDamping=damping, angularDamping=damping)
+damping = 0.1  # Damping factor
+for i in range(num_joints):
+    p.changeDynamics(robotId, i, linearDamping=damping, angularDamping=1)
     
 # Main simulation loop
 while True:
@@ -84,5 +155,10 @@ while True:
         state = p.getJointState(robotId, i)
         joint_states.append(state[0])  # state[0] is the current position
     # print(f"Current joint positions: {joint_states}")
-
+    
+    # keys = p.getKeyboardEvents(physicsClient)
+    # if ord('q') in keys:
+    #     print("Exiting...")
+    #     break
+    # print(keys)
 p.disconnect()
